451d6a6afbd98788b717b3281c56c67b676e0df2
Juliano Penna <julianopenna@gmail.com>
2019-05-16 20:09:12 +0800

    Add cors

--- server/index.ts
+++ server/index.ts
@@ -1,12 +1,15 @@
 import 'dotenv/config';
 import express from 'express';
 import morgan from 'morgan';
+import cors from 'cors';
 
 import './db/connect';
 import apiV1 from './api/v1';
 
 // Create a new express application instance
 const app: express.Application = express();
+
+app.use(cors());
 
 app.use(express.json());
 if (process.env.NODE_ENV !== 'production') app.use(morgan('dev'));
