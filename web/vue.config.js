b0f21ddfb341f046f301fe3ed61fc87be21fdd7e
Juliano Penna <julianopenna@gmail.com>
2019-05-21 22:56:47 +0800

    Simplify updating the app (sw)

--- web/vue.config.js
+++ web/vue.config.js
@@ -2,20 +2,11 @@ function escapeRegExp(string) {
   return string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&'); // $& means the whole matched string
 }
 
-// module.exports = {
-//   pwa: {
-//     workboxOptions: {
-//       runtimeCaching: [{
-//         urlPattern: new RegExp(`${escapeRegExp(process.env.VUE_APP_API_URL)}`),
-//         handler: 'staleWhileRevalidate',
-//       }],
-//     },
-//   },
-// };
-
 module.exports = {
   pwa: {
     workboxOptions: {
+      skipWaiting: true,
+      clientsClaim: true,
       runtimeCaching: [{
         urlPattern: new RegExp(`${escapeRegExp(process.env.VUE_APP_API_URL)}`),
         handler: 'networkFirst',
