b213daf514e894a14f8ecbb163fa87fde02df9b5
Juliano Penna <julianopenna@gmail.com>
2019-05-21 21:06:42 +0800

    Change pwa strategy, network first

--- web/vue.config.js
+++ web/vue.config.js
@@ -2,27 +2,27 @@ function escapeRegExp(string) {
   return string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&'); // $& means the whole matched string
 }
 
-module.exports = {
-  pwa: {
-    workboxOptions: {
-      runtimeCaching: [{
-        urlPattern: new RegExp(`${escapeRegExp(process.env.VUE_APP_API_URL)}`),
-        handler: 'staleWhileRevalidate',
-      }],
-    },
-  },
-};
-
 // module.exports = {
 //   pwa: {
 //     workboxOptions: {
 //       runtimeCaching: [{
 //         urlPattern: new RegExp(`${escapeRegExp(process.env.VUE_APP_API_URL)}`),
-//         handler: 'networkFirst',
-//         options: {
-//           networkTimeoutSeconds: 5,
-//         },
+//         handler: 'staleWhileRevalidate',
 //       }],
 //     },
 //   },
 // };
+
+module.exports = {
+  pwa: {
+    workboxOptions: {
+      runtimeCaching: [{
+        urlPattern: new RegExp(`${escapeRegExp(process.env.VUE_APP_API_URL)}`),
+        handler: 'networkFirst',
+        options: {
+          networkTimeoutSeconds: 10,
+        },
+      }],
+    },
+  },
+};
